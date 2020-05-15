b356dfbdf721ae18ee4e9f30549d068dc8861a52
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-15 11:28:25 -0300

    Leitura dos dados

--- src/problem.java
+++ src/problem.java
@@ -7,9 +7,18 @@ public class problem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
+		double salario, serviços, capital, medicos, educacionais;
 		
-		
-		
+		System.out.println("Renda anual com salário: ");
+		salario = sc.nextDouble();
+		System.out.println("Renda anual com prestação de serviço: ");
+		serviços = sc.nextDouble();
+		System.out.println("Renda anual com ganho de capital: ");
+		capital = sc.nextDouble();
+		System.out.println("Gastos médicos: ");
+		medicos = sc.nextDouble();
+		System.out.println("Gastos educacionais: ");
+		educacionais = sc.nextDouble();
 		
 		sc.close();
 	}
